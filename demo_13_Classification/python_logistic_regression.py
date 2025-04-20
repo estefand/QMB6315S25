2ebe1f55cbea703e9254c4b6913f3b0e0a1bb0eb
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2025-04-20 17:00:45 -0400

    Revised script to output png images for display in GitHub

--- demo_13_Classification/python_logistic_regression.py
+++ demo_13_Classification/python_logistic_regression.py
@@ -225,7 +225,7 @@ from sklearn.metrics import roc_curve
 logit_roc_auc = roc_auc_score(y, logit_model_fit_sk.predict_proba(X)[:,1])
 fpr, tpr, thresholds = roc_curve(y, logit_model_fit_sk.predict_proba(X)[:,1])
 
-# Plot the ROC curve.
+# Plot the ROC curve and save in a pdf image.
 plt.figure()
 plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
 plt.plot([0, 1], [0, 1],'r--')
@@ -238,6 +238,23 @@ plt.legend(loc="lower right")
 plt.savefig('Logit_ROC.pdf')
 plt.show()
 
+
+
+# Plot another ROC curve in png format.
+# Although pdf images are higher quality.
+# images in png format will display on GitHub. 
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Logit_ROC.png')
+plt.show()
+# The only difference is the extension in the file name.
 
 
 ##################################################
